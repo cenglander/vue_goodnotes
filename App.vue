e138c02dec8e89ae02464440f89b01d5678ee4e6
Charlotte Englander <charlotte.englander@gmail.com>
2020-05-06 09:58:51 +0200

    First commit

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+<main>
+
+</main>
+</template>
+
+<script>
+import Child from './Child'
+export default {
+    name: 'App',
+    components: {
+        Child: Child
+    },
+}
+</script>
+
+<style>
+    body {
+        background: #ECE4B7;
+    }
+
+</style>
